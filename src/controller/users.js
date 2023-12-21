33d1b2d78e01665857ef31b836d0e861f703adf2
Erlang Andriyanputra <chilliope48@gmail.com>
2023-12-21 13:06:12 +0800

    crud, static file, upload file

--- src/controller/users.js
+++ src/controller/users.js
@@ -19,9 +19,16 @@ const getAllUsers = async (req, res) => {
 const createNewUser = async (req, res) => {
     const {body} = req;
     
+    if(!body.email || !body.name || !body.address) {
+        return res.status(400).json({
+            message: 'Anda mengirimkan data yang salah',
+            data: null
+        })
+    }
+
     try {
         await UsersModel.createNewUser(body)
-        res.json({
+        res.status(201).json({
             message: 'CREATE New User Success',
             data: req.body
         })
@@ -33,25 +40,40 @@ const createNewUser = async (req, res) => {
     }
 }
 
-const updateUser = (req, res) => {
+const updateUser = async (req, res) => {
     const {id} = req.params
-    console.log(req.params)
-    res.json({
-        message: 'UPDATE user success',
-        data: req.body
-    })
+    const {body} = req
+    try {
+        await UsersModel.updateUser(body, id)
+        res.json({
+            message: 'UPDATE user success',
+            data: {
+                id: id,
+                ...body
+            }
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: 'Server Error',
+            serverMessage: error,
+        })
+    }
 }
 
-const deleteUser = (req, res) => {
+const deleteUser = async (req, res) => {
     const {id} = req.params
-    res.json({
-        message: 'DELETE user success',
-        data: {
-            id: id,
-            name: 'Mr.Lorem',
-            address: 'Bandung'
-        }
-    })
+    try {
+        await UsersModel.deleteUser(id)
+        res.json({
+            message: 'DELETE user success',
+            data: null
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: 'Server Error',
+            serverMessage: error,
+        })
+    }
 }
 
 module.exports = {
--- src/index.js
+++ src/index.js
@@ -5,13 +5,27 @@ const express = require('express')
 const usersRoutes = require('./routes/users')
 
 const middlewareLogRequest = require('./middleware/log')
+const upload = require('./middleware/multer')
 
 const app = express()
 
 app.use(middlewareLogRequest)
 app.use(express.json())
+app.use('/assets', express.static('public/images/'))
 
 app.use('/users', usersRoutes)
+app.post('/upload',upload.single('photo'), (req, res) => {
+    res.json({
+        message: 'Upload Berhasil'
+    })
+})
+
+// error handling
+app.use((err, req, res, next) => {
+    res.json({
+        message: err.message
+    })
+})
 
 app.listen(PORT, () => {
     console.log(`Server running at Port ${PORT}`)
